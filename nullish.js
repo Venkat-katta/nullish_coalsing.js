dc66d1a7a75a72060a4209bed8f7693c091e184d
Venkat-katta <prasadkatta4415@gmail.com>
2025-01-27 12:22:06 +0530

    changes added

--- nullish.js
+++ nullish.js
@@ -1,17 +1,18 @@
-let html = prompt("are you aware of html(yes/no)") . toLowercase() === "yes";
-let css = prompt("are you aware of css(yes/no)") . toLowercase() === "yes";
-let boothcamp = prompt("are you aware of boothcamp(yes/no)") . tolowercase() === "yes";
-let js = prompt("are you aware of js(yes/no)") . toLowercase() === "yes";
-let react = prompt("are you aware of react(yes/no)") . toLowercase() === "yes";
-let node = prompt("are you aware of node(yes/no)") . toLowercase() === "yes";
-result=
-         html && css && boothcamp && !js && !node && !react 
-         ? "you can became UI,UX Designer"
-        :  html && css && boothcamp && !js && !node && !react 
-        ? "you can became Frontend Developer"
-        :  html && css && boothcamp && !js && !node && !react
-        ? "you can become Backend developer"
-        :  html && css && boothcamp && !js && !node && !react
-        ? "you can become Full Stack Developer"
-        : "You Want To Learn This Skills, Join 10000Coders";
-        alert(result)
+let html = prompt("Are you aware of HTML(Yes/No)").toLowerCase() === "yes";
+let css = prompt("Are you aware of CSS(Yes/No)").toLowerCase() === "yes";
+let bootstrap =
+  prompt("Are you aware of BOOTSTRAP(Yes/No)").toLowerCase() === "yes";
+let js = prompt("Are you aware of JS(Yes/No)").toLowerCase() === "yes";
+let react = prompt("Are you aware of REACT(Yes/No)").toLowerCase() === "yes";
+let node = prompt("Are you aware of NODE(Yes/No)").toLowerCase() === "yes";
+result =
+  html && css && bootstrap && !js && !react && !node
+    ? "you can became UI,UX Designer"
+    : html && css && bootstrap && js && react && !node
+    ? "You can become a FrontEnd Developer"
+    : html && css && bootstrap && js && !react && node
+    ? "You can become a Backend Developer"
+    : html && css && bootstrap && js && react && node
+    ? "You can become a Fullstack Developer"
+    : "You Want To Learn This Skills, Join 10000Coders";
+alert(result);
